ff015e415a77593daa735a968e48825353af849d
hangwin <yzhangwin@gmail.com>
2022-08-18 00:54:16 +0800

    chore: 修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,38 @@ export default defineConfig(({ mode }) => {
 				},
 			],
 		},
+		build: {
+			// 我们的构建产物需要兼容到es6
+			target: 'es2015',
+			// 假如要兼容安卓微信的webview
+			cssTarget: 'chrome61',
+			// 非生产环境下生成sourcemap
+			sourcemap: mode !== 'prod',
+			// 禁用gzip 压缩大小报告，因为压缩大型文件可能会很慢
+			reportCompressedSize: false,
+			// chunk大小超过1500kb是触发警告
+			chunkSizeWarningLimit: 1500,
+		},
+		server: {
+			// 开启https
+			https: true,
+			// 监听所有ip地址
+			host: true,
+			// 端口默认是5173
+			port: 6666,
+			// 配置代理帮我们转发请求，解决跨域问题
+			proxy: {
+				// api/开头的请求将被转发到下面的target的地址
+				'api/': {
+					target: 'https://mock.com',
+					// 改变请求头的origin
+					changeOrigin: true,
+					// 支持代理websocket
+					ws: true,
+					// 路径重写 相当于把api/去掉
+					rewrite: (path) => path.replace(new RegExp(`^api/`), ''),
+				},
+			},
+		},
 	};
 });
