acfa1e12d0cd549f36b6887e58db1fcb9e31ceda
hangwin <yzhangwin@gmail.com>
2022-09-07 02:42:07 +0800

    feat: 颜色修改

--- src/theme/colors/colorBase.ts
+++ src/theme/colors/colorBase.ts
@@ -15,11 +15,64 @@ export const baseColor = {
 	black: '#000000',
 };
 
+// 基础色，用于生成不同色阶的调色板
+export const baseColor2 = {
+	// 主色 通常对应主题色
+	// 紫色，用作主题色和功能色中的提示色
+	purple: '#605BFF',
+
+	// 辅助色 通常就是功能色
+	// 绿色，用作成功色
+	green: '#00B30F',
+	// 黄色 警告色
+	yellow: '#FF9500',
+	// 红色 错误色
+	red: '#FF1500',
+
+	// 中性色  黑白灰 用作背景、文字、边框等颜色
+	// 灰色 用作背景色等
+	gray: [
+		'#fafafa',
+		'#f5f5f5',
+		'#f0f0f0',
+		'#d9d9d9',
+		'#bfbfbf',
+		'#8c8c8c',
+		'#595959',
+		'#434343',
+		'#262626',
+		'#1f1f1f',
+		'#141414',
+	],
+	// 白色，深色背景下的文字颜色
+	white: '#FFFFFF',
+	black: '#000000',
+};
+
 // 通过color衍生出其他过渡色
 export const getColors = (mode: 'dark' | 'light' = 'light', color: string = baseColor.primary) => {
 	const antMode = mode === 'light' ? 'default' : mode;
-	const colors = generate(color, { theme: antMode });
+	const options: any = { theme: antMode };
+	if (antMode === 'dark') {
+		options.backgroundColor = '#141414';
+	}
+	const colors = generate(color, options);
 	return colors;
+};
+
+export const lightColors2 = {
+	purples: getColors('light', baseColor2.purple),
+
+	greens: getColors('light', baseColor2.green),
+	yellows: getColors('light', baseColor2.yellow),
+	reds: getColors('light', baseColor2.red),
+};
+export const darkColors2 = {
+	purples: getColors('dark', baseColor2.purple),
+
+	greens: getColors('dark', baseColor2.green),
+	yellows: getColors('dark', baseColor2.yellow),
+	reds: getColors('dark', baseColor2.red),
 };
 
 export const lightColors = {
--- src/theme/colors/colorVariable.ts
+++ src/theme/colors/colorVariable.ts
@@ -1,4 +1,4 @@
-import { lightColors, darkColors } from './colorBase';
+import { lightColors, darkColors, lightColors2, darkColors2, baseColor2 } from './colorBase';
 //  用于生成颜色变量
 export const getColorVariables = (theme: 'light' | 'dark' = 'light', primaryColor?: string[]) => {
 	const colors = theme === 'light' ? { ...lightColors } : { ...darkColors };
@@ -43,5 +43,74 @@ export const getColorVariables = (theme: 'light' | 'dark' = 'light', primaryColo
 
 		// 边框颜色
 		'--h-border-color': colors.bg[6],
+	};
+};
+
+//  用于生成颜色变量
+export const getColorVariables2 = (theme: 'light' | 'dark' = 'light', primaryColor?: string[]) => {
+	const colors: any = theme === 'light' ? { ...lightColors2 } : { ...darkColors2 };
+	const isDark = theme === 'dark';
+	if (primaryColor) {
+		colors.primary = primaryColor;
+	} else {
+		colors.primary = colors.purples;
+	}
+	console.log('getColorVariables', colors);
+	return {
+		// 主题色
+		'--h-primary': colors.primarys[5],
+		'--h-primary-hover': colors.primarys[4],
+		'--h-primary-active': colors.primarys[6],
+		'--h-primary-disabled': colors.primarys[3],
+
+		'--h-primary-light-1': colors.primarys[0],
+		'--h-primary-light-2': colors.primarys[1],
+		'--h-primary-light-3': colors.primarys[2],
+		'--h-primary-light-4': colors.primarys[3],
+		'--h-primary-light-5': colors.primarys[4],
+		'--h-primary-light-6': colors.primarys[5],
+		'--h-primary-light-7': colors.primarys[6],
+		'--h-primary-light-8': colors.primarys[7],
+		'--h-primary-light-9': colors.primarys[8],
+
+		// 功能色
+		// 提示
+		'--h-info': colors.primarys[5],
+		'--h-info-hover': colors.primarys[4],
+		'--h-info-active': colors.primarys[6],
+		'--h-info-disabled': colors.primarys[3],
+		// 成功
+		'--h-success': colors.greens[5],
+		'--h-success-hover': colors.greens[4],
+		'--h-success-active': colors.greens[6],
+		'--h-success-disabled': colors.greens[3],
+		// 警告
+		'--h-warn': colors.yellows[5],
+		'--h-warn-hover': colors.yellows[4],
+		'--h-warn-active': colors.yellows[6],
+		'--h-warn-disabled': colors.yellows[3],
+		// 错误
+		'--h-danger': colors.reds[5],
+		'--h-danger-hover': colors.reds[4],
+		'--h-danger-active': colors.reds[6],
+		'--h-danger-disabled': colors.reds[3],
+
+		// 文字
+		'--h-text-color-title': isDark ? baseColor2.gray[0] : baseColor2.gray[10],
+		'--h-text-color-primary': isDark ? baseColor2.gray[1] : baseColor2.gray[9],
+		'--h-text-color-secondary': isDark ? baseColor2.gray[2] : baseColor2.gray[8],
+		'--h-text-color-disabled': isDark ? baseColor2.gray[3] : baseColor2.gray[7],
+
+		// 边框颜色
+		'--h-border-color': isDark ? baseColor2.gray[4] : baseColor2.gray[5],
+		// 分割线
+		'--h-divider-color': isDark ? baseColor2.gray[6] : baseColor2.gray[4],
+		// 背景填充色
+		'--h-fill-bg': isDark ? baseColor2.gray[0] : baseColor2.gray[10],
+		'--h-fill-bg-hover': isDark ? baseColor2.gray[1] : baseColor2.gray[9],
+
+		'--h-fill-form-bg': isDark ? baseColor2.white : baseColor2.black,
+
+		'--h-fill-input-bg': colors.bg[5],
 	};
 };
