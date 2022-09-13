1398e2eac785a9079ed17912c5e2521d03c7ca95
hangwin <yzhangwin@gmail.com>
2022-09-13 21:19:41 +0800

    feat: dashboard临时性提交

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,19 @@
 <template>
-	<div>{{ userStore.token }}</div>
+	<el-container>
+		<el-container>
+			<el-aside>侧边菜单</el-aside>
+			<el-container>
+				<el-header>头部</el-header>
+				<el-main
+					>主要内容区<div>{{ userStore.token }}</div></el-main
+				>
+				<el-footer>脚部</el-footer>
+			</el-container>
+		</el-container>
+	</el-container>
 </template>
 <script lang="ts" setup>
+import { ElContainer, ElHeader, ElMain, ElFooter, ElAside } from 'element-plus';
 import { useUserStore } from '@/store/modules/user';
 const userStore = useUserStore();
 </script>
